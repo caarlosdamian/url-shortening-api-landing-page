a94bd1a4c5c77d8c26565ef61825b5e9ba13052f
caarlosdamian <dapecarlos@gmail.com>
2023-12-10 22:00:09 -0600

    feat(components): added header mobile

--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import Image from 'next/image';
+import logo from '../../images/logo.svg';
+import hamburger from '../../images/hamburger.svg';
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex justify-between w-full px-6 pb-6 pt-10">
+      <div className="">
+        <Image
+          src={logo}
+          width={120}
+          height={32}
+          alt="logo"
+          className="cursor-pointer"
+        />
+      </div>
+      <div className="">
+        <Image
+          src={hamburger}
+          width={24}
+          height={21}
+          alt="hamburger"
+          className="cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
