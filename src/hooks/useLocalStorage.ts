23305b00b9c8fffee323d4d929357a20b6ce3d5d
caarlosdamian <dapecarlos@gmail.com>
2023-12-20 22:58:18 -0600

    feat(hooks): added new custom hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,30 @@
+'use client'
+import { useEffect, useState } from 'react';
+
+export const useLocalStorage = ({
+  key,
+  initValue,
+}: {
+  key: string;
+  initValue: any;
+}) => {
+    const [value, setValue] = useState(() => {
+        try {
+          const storedValue = window.localStorage.getItem(key);
+          return storedValue ? JSON.parse(storedValue) : initValue;
+        } catch (error) {
+          console.error(error);
+          return undefined;
+        }
+      });
+    
+      useEffect(() => {
+        try {
+          window.localStorage.setItem(key, JSON.stringify(value));
+        } catch (error) {
+          console.error(error);
+        }
+      }, [key, value]);
+    
+      return [value, setValue];
+};
--- src/sections/actions/Actions.tsx
+++ src/sections/actions/Actions.tsx
@@ -1,6 +1,8 @@
+'use client';
 import { Button, Card, Feature, Input } from '@/components';
 import { sampleFeature, sampleLinks } from '@/constants';
 import { Link } from '@/constants/types';
+import { useLocalStorage } from '@/hooks/useLocalStorage';
 import Image from 'next/image';
 import React from 'react';
 
@@ -8,7 +10,7 @@ export const Actions = () => {
   return (
     <section className="col-start-1 col-end-4 pb-20 bg-primary-flashWhite min-h-screen mt-40 lg:mt-0">
       <div className="relative">
-        <div className="absolute rounded-[10px] bg-no-repeat top-[-80px] left-0 right-0 mx-auto flex flex-col gap-4 bg-primary-jacarta w-[327px] min-h-[160px] overflow-hidden p-6 sm:w-full sm:max-w-[1110px] sm:flex-row sm:items-center">
+        <form className="absolute rounded-[10px] bg-no-repeat top-[-80px] left-0 right-0 mx-auto flex flex-col gap-4 bg-primary-jacarta w-[327px] min-h-[160px] overflow-hidden p-6 sm:w-full sm:max-w-[1110px] sm:flex-row sm:items-center">
           <Input
             placeholder="Shorten a link here..."
             otherClassName="sm:w-full sm:h-[64px]"
@@ -16,6 +18,7 @@ export const Actions = () => {
           <Button
             label="Shorten It!"
             otherClassNames="z-30 sm:max-w-[188px] sm:p-[18px] sm:min-h-[64px]"
+            type="submit"
           />
           <Image
             src="/images/bg-shorten-mobile.svg"
@@ -31,7 +34,7 @@ export const Actions = () => {
             height={0}
             className="hidden sm:inline sm:absolute z-0 w-full h-full right-0 top-0"
           />
-        </div>
+        </form>
       </div>
       <div className="w-full pt-[128px] flex flex-col gap-12 break-words px-6">
         <div className="flex flex-col gap-6">
@@ -49,7 +52,7 @@ export const Actions = () => {
           </h4>
         </div>
         <div className="flex flex-col gap-[92px] mt-[44px]">
-          {sampleFeature.map((item)=>(
+          {sampleFeature.map((item) => (
             <Feature key={item.id} feature={item} />
           ))}
         </div>
