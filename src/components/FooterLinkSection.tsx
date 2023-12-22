8e76fb7f935cad01cd64d7b3e6d673746da1faa5
caarlosdamian <dapecarlos@gmail.com>
2023-12-21 18:55:00 -0600

    feat(section): added footer section desktop desing

--- src/components/FooterLinkSection.tsx
+++ src/components/FooterLinkSection.tsx
@@ -7,12 +7,12 @@ interface props {
 
 export const FooterLinkSection = ({ item: { links, title } }: props) => {
   return (
-    <div className="flex items-center flex-col gap-6 ">
+    <div className="flex items-center flex-col gap-6 md:items-start">
       <h3 className="text-white text-base font-bold">{title}</h3>
-      <div className="flex flex-col items-center gap-[10px]">
+      <div className="flex flex-col items-center gap-[10px] md:items-start">
         {links.map((link) => (
           <span
-            className="text-[15px] font-medium text-[#BFBFBF]"
+            className="text-[15px] font-medium text-[#BFBFBF] hover:text-primary-blueGreen cursor-pointer"
             key={link.id}
           >
             {link.title}
--- src/sections/footer/Footer.tsx
+++ src/sections/footer/Footer.tsx
@@ -5,14 +5,14 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <div className="flex items-center flex-col justify-center col-start-1 col-end-4 bg-[#232127] py-14 px-[104px gap-12">
+    <div className="flex items-center flex-col justify-center col-start-1 col-end-4 bg-[#232127] py-14 px-[104px gap-12 md:flex-row md:justify-between md:px-[165px] md:py-[74px] md:items-start">
       <Image
         src="/images/logo-white.svg"
         alt="footer-logo"
         width={120}
         height={32}
       />
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-6 md:flex-row md:gap-28">
         {footerLinks.map((link) => (
           <FooterLinkSection key={link.id} item={link} />
         ))}
